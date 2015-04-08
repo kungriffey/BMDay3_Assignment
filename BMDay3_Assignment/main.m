949546b4d1d1ad2f0de936503d43f485e766d671
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-08 14:49:16 -0400

    Completed Challenge One!

--- BMDay3_Assignment/main.m
+++ BMDay3_Assignment/main.m
@@ -8,13 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+float convertToCelsius() {
+    
+    float tempInFahrenheit = 27;
+    float tempInCelsius = ((tempInFahrenheit - 32) / 1.8);
+    return tempInCelsius;
+}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
-    
-    
-    
-    
+        float answer = convertToCelsius();
+        NSLog(@"27F is %fC", answer); //how can we round it to 2 decimal places
     
     }
     return 0;
