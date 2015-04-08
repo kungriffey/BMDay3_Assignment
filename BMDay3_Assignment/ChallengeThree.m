8ee70dc68f56e61c2f9d4c9e3c4ae903376ac770
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-08 15:15:55 -0400

    completed Challenge three!

--- BMDay3_Assignment/ChallengeThree.m
+++ BMDay3_Assignment/ChallengeThree.m
@@ -8,11 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+
+int computeSumFunction(int a, int b){
+    
+    
+    int c = a + b;
+    
+    return c;
+}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        
-
+        int answer = computeSumFunction(10, 5);
+        NSLog(@"10 + 5 is %i", answer);
         
     }
     return 0;
